e031a9448603d72fbe449f61ef624c6aa5802d5c
wuqingvika <526288022@qq.com>
2018-05-08 22:16:39 +0800

    订单、商品分层模式子工程、mybatis生成Pojo、mapper文件maven项目

---
+++ wq-single-mvc/wq-single-mvc-mapper/src/main/java/com/wq/mapper/OrdersMapper.java
@@ -0,0 +1,32 @@
+package com.wq.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.wq.pojo.Orders;
+import com.wq.pojo.OrdersExample;
+
+public interface OrdersMapper {
+    int countByExample(OrdersExample example);
+
+    int deleteByExample(OrdersExample example);
+
+    int deleteByPrimaryKey(String id);
+
+    int insert(Orders record);
+
+    int insertSelective(Orders record);
+
+    List<Orders> selectByExample(OrdersExample example);
+
+    Orders selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByExample(@Param("record") Orders record, @Param("example") OrdersExample example);
+
+    int updateByPrimaryKeySelective(Orders record);
+
+    int updateByPrimaryKey(Orders record);
+}
