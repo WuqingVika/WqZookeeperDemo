f1e8e5de9ec5f1cd7813b2a20d534a9c9208f687
wuqingvika <526288022@qq.com>
2018-05-11 23:45:53 +0800

    使用jar包方式 记住要用管理员身份运行cmd

--- wq-dubbo/wq-dubbo-item-service/src/main/java/com/wq/item/starter/ItemApplication.java
+++ wq-dubbo/wq-dubbo-item-service/src/main/java/com/wq/item/starter/ItemApplication.java
@@ -3,7 +3,7 @@ package com.wq.item.starter;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ItemApplication {
-//main线程启动dubbo服务
+//main线程启动dubbo服务 第二种启动方式
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] {"classpath:spring/spring-context.xml"});
